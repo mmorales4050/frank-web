cf847fbe5f1d2164c63dbab39fd78f32f91899e2
mmorales4050 <mmorales4050@gmail.com>
2019-08-16 17:35:17 -0400

    add first review

--- src/components/ReviewPage.js
+++ src/components/ReviewPage.js
@@ -1,22 +1,23 @@
 import React, { Component } from 'react';
-import { List } from 'semantic-ui-react'
+import { List, Icon } from 'semantic-ui-react'
 
 class ReviewPage extends Component {
-  reviews = [
-    {
-      stars: 5,
-      date: "08/03/2019",
-      
-    }
-  ]
+
   render() {
     return (
       <List divided relaxed id="review-list">
     <List.Item>
-      <List.Icon name='github' size='large' verticalAlign='middle' />
       <List.Content>
-        <List.Header as='a'>Semantic-Org/Semantic-UI</List.Header>
-        <List.Description as='a'>Updated 10 mins ago</List.Description>
+        <List.Header><Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/>Highly likely to recommend</List.Header>
+        <List.Description>08/03/2019 - David Logan</List.Description>
+        <List.Description>Showed home in 2018 in South Lakes Dr - Soapstone Dr, Reston, VA 20191.</List.Description>
+        <List.Description>Local knowledge: <Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/></List.Description>
+        <List.Description>Process expertise: <Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/></List.Description>
+        <List.Description>Responsiveness: <Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/></List.Description>
+        <List.Description>Negotiation skills: <Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/><Icon name="star"/></List.Description>
+        <List.Description>
+        Frank was very reliable and on time. He was extremely knowledgeable about the entire Reston-Herndon area and I would recommend him to all my friends and family.
+        </List.Description>
       </List.Content>
     </List.Item>
     <List.Item>
