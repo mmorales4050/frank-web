41347384d125e86e8fef00a73d31525cf5e31c45
mmorales4050 <mmorales4050@gmail.com>
2019-08-16 18:16:08 -0400

    add review image

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { List, Icon } from 'semantic-ui-react'
+import './Review.css'
 
 class Review extends Component {
 
   render() {
     return (
       <List.Item>
-      <Icon name="star"></Icon>
+      <img src="review1.png" className="review-image"/>
+      <div className="review-text">
+      Frank was very reliable and on time. He was extremely knowledgeable about the entire Reston-Herndon area and I would recommend him to all my friends and family.
+      </div>
       </List.Item>
     );
   }
